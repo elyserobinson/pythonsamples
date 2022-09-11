b95ed490e516a482831fda080024a9d7b967b078
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2022-09-11 13:54:29 -0500

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -9,3 +9,26 @@ x3 = num - x * 1000 - x1 * 100 - x2 * 10
 
 # Display results
 print("The sum of the digits are", x + x1 + x2 + x3)
+
+
+
+
+
+
+# Enter the first point with two float values
+x1, y1 = eval(input("Enter x1 and y1 for Point 1: "))
+
+# Enter the second point with two float values
+x2, y2 = eval(input("Enter x2 and y2 for Point 2: "))
+
+# Enter the third point with two float values
+x3, y3 = eval(input("Enter x2 and y2 for Point 3: "))
+
+# Computer the area of the triangle
+side1 = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+side2 = ((x3 - x1) ** 2 + (y3 - y1) ** 2) ** 0.5
+side3 = ((x2 - x3) ** 2 + (y2 - y3) ** 2) ** 0.5
+s = (side1 + side2 + side3) / 2
+area = round((s * (s - side1) * (s - side2) * (s - side3)) ** 0.5, 2)
+
+print("The area of the triangle is", round(area, 2))

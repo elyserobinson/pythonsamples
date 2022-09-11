60e5bbe3196ee6a29cdd21958d6fc5b6cf91cb04
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2022-09-11 13:31:29 -0500

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -1,8 +1,11 @@
+# Enter a number between 0 and 1000
 num = int(input("Enter a number between 0 and 1000: "))
 
+# Add up the numbers
 x = num // 1000
 x1 = (num - x * 1000) // 100
 x2 = (num - x * 1000 - x1 * 100) // 10
 x3 = num - x * 1000 - x1 * 100 - x2 * 10
 
+# Display results
 print("The sum of the digits are", x + x1 + x2 + x3)

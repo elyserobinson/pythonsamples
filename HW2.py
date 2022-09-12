5997e88b6d95a46bee89fdb92798a05b6242ea7f
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2022-09-11 19:24:38 -0500

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -1,3 +1,42 @@
+'''
+   CIST 005A Fall 2022
+   HW Week 2 Chapter 2 Problem 2.1
+   Description: Write a program that reads a Celsius degree from the console and converts it to Fahrenheit and displays the result.
+   Input: any number
+   Output: Fahrenheit
+   Student: Elyse Y. Robinson
+   Known bugs: none.
+   Date: September 6, 2022
+'''
+
+# Prompt the user to enter a number
+celsius = eval(input("Enter any number for celsius: "))
+
+# The formula to convert celsius to fahrenheit
+fahrenheit = (9 / 5) * celsius + 32
+
+# Display fahrenheit
+print(celsius, "degrees celsius is", fahrenheit, "degrees fahrenheit.")
+
+'''
+Enter any number for celsius: 
+0
+0 degrees celsius is 32.0 degrees fahrenheit.
+'''
+
+print('')
+
+'''
+   CIST 005A Fall 2022
+   HW Week 2 Chapter 2 Problem 2.6
+   Description: Write a program that reads an integer between 0 and 1000 and adds all the digits in the integer.
+   Input: A number between 0 and 1000
+   Output: Adding of all numbers from the input
+   Student: Elyse Y. Robinson
+   Known bugs: none.
+   Date: September 6, 2022
+'''
+
 # Enter a number between 0 and 1000
 num = int(input("Enter a number between 0 and 1000: "))
 
@@ -11,9 +50,24 @@ x3 = num - x * 1000 - x1 * 100 - x2 * 10
 print("The sum of the digits are", x + x1 + x2 + x3)
 
 
+'''
+Enter a number between 0 and 1000: 
+333
+The sum of the digits are 9
+'''
 
+print('')
 
-
+'''
+   CIST 005A Fall 2022
+   HW Week 2 Chapter 2 Problem 2.14
+   Description: Write a program that prompts the user to enter the three points of a triangle and displays its area.
+   Input: 3 points
+   Output: Area of a triangle
+   Student: Elyse Y. Robinson
+   Known bugs: none.
+   Date: September 6, 2022
+'''
 
 # Enter the first point with two float values
 x1, y1 = eval(input("Enter x1 and y1 for Point 1: "))
@@ -32,3 +86,13 @@ s = (side1 + side2 + side3) / 2
 area = round((s * (s - side1) * (s - side2) * (s - side3)) ** 0.5, 2)
 
 print("The area of the triangle is", round(area, 2))
+
+'''
+Enter x1 and y1 for Point 1: 
+1.5, -3.4
+Enter x2 and y2 for Point 2: 
+4.6, 5
+Enter x2 and y2 for Point 3: 
+9.5, -3.4
+The area of the triangle is 33.6
+'''

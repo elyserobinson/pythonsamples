96a0c32cc858d892d6cd9021862afd378b244994
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2022-09-15 18:00:08 -0500

    Create HW3.py

---
+++ HW3.py
@@ -0,0 +1,11 @@
+# A program that receives an ASCII code between 0 and 127 and displays its characters.
+
+ascii = eval(input("Enter a number between 0 and 127: "))
+code = chr(ascii)
+
+print("The character is ", code)
+
+
+
+
+

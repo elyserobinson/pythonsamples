411976c340394298e8cc6551fd26c161b19bd695
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2022-09-22 17:59:42 -0500

    Create TestBooleanOperators.py

---
+++ TestBooleanOperators.py
@@ -0,0 +1,14 @@
+# The program in checks whether a number is divisible by 2 and 3, by 2 or 3, and by 2 or 3 but not both.
+
+# Receive an input
+number = eval(input("Enter an integer: "))
+
+if number % 2 == 0 and number % 3 == 0:
+  print(number, "is divisble by 2 and 3")
+
+if number % 2 == 0 or number % 3 == 0:
+  print(number, "is divisble by 2 or 3")
+
+if (number % 2 == 0 or number % 3 == 0) and \
+not (number % 2 == 0 and number % 3 == 0):
+  print(number, "is divisble by 2 or 3, but not both")

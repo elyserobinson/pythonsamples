4834cad487d88f9b10a80b310118ca55c9557eda
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2022-09-22 15:31:14 -0500

    Create AdditionQuiz.py

---
+++ AdditionQuiz.py
@@ -0,0 +1,13 @@
+# Generate two single digit numbers and prompt the user to answer the question and check whether the student's answer is correct.
+
+import random
+
+# Generate random numbers
+number1 = random.randint(0, 9)
+number2 = random.randint(0, 9)
+
+# Prompt the user to enter an answer
+answer = eval(input("What is " + str(number1) + " + " + str(number2) + "? "))
+
+# Display result
+print(number1, "+", number2, "=", answer, "is", number1 + number2 == answer)

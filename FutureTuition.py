90f36248feefeffad689315c4e43671148da8120
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2022-10-01 12:23:38 -0500

    Create FutureTuition.py

---
+++ FutureTuition.py
@@ -0,0 +1,11 @@
+# A program that doubles the tuition every year.
+
+year = 0 # Year 0
+tuition = 10000
+
+while tuition < 20000:
+    year += 1 
+    tuition = tuition * 1.07
+    
+print("Tuition will be doubled in", year, "years.")
+print("Tuition will be $" + format(tuition, ".2f"), "in", year, "years.")

737edba80ae90cba2bc1ae5112061d143bdffb4d
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2022-10-09 14:08:41 -0500

    Create DefaultArgumentDemo.py

---
+++ DefaultArgumentDemo.py
@@ -0,0 +1,11 @@
+# Demonstrates how to define functions with default argument values and how to invoke such functions.
+
+def printArea(width = 1, height = 2):
+    area = width * height
+    print("width:", width, "\theight:", height, "\tarea:", area)
+
+printArea() # Default arguments width = 1 and height = 2
+printArea(4, 2.5) # Positional arguments width = 4 and height = 2.5
+printArea(height = 5, width = 3) # Keyword arguments width
+printArea(width = 1.2) # Default height = 2
+printArea(height = 6.2) # Default width = 1
